42b9f84a7f99c90914fcf1a386deaea489f93c95
Gustavo Allen <gustavoallen92@gmail.com>
2020-11-07 04:18:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,51 @@
+def turn(board)
+  puts "Welcome to Tic Tac Toe!"
+
+  display_board(board)
+
+  puts "Please enter 1-9:"
+  move.get.chomp
+
+  if valid_move?
+    index = input_to_index(input)
+    move(board, index)
+  else
+    turn(board)
+
+
+  
+end
+
 def display_board(board)
   puts " board[0] | board[1] | board[2] "
   puts "-----------"
   puts " board[3] | board[4] | board[5] "
   puts "-----------"
   puts " board[6] | board[7] | board[8] "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def vald_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    true
+  elsif board[index] == "X" || board[index] == "O"
+    false
+  else
+    false
+  end
+end
+
+def move(board, index, character = "X")
+  board[index] = character
 end
